11db704e76e9ff74d4517bf6c8ebe869c8c8f93c
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-17 13:18:57 +0100

    Connect the Redux store to my app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import BookForm from '../containers/BooksForm';
-import BookList from '../containers/BooksList';
+import { Provider } from 'react-redux';
+import store from './store';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 
 function App() {
   return (
-    <div>
-      <BookForm />,
-      <BookList />
-    </div>
+    <Provider store={store}>
+      <div>
+        <BooksList />
+        <BooksForm />
+      </div>
+    </Provider>
   )
 }
 
