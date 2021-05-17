de79b06a21256bf4e458012c97c8e9c0ada3dc81
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-17 13:57:26 +0100

    Add simple form for adding books

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+const CATEGORIES = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
 function BooksForm() {
-  return(
+  return (
     <div>
-      <h3>Books Form</h3>
+      <h2>Books Form</h2>
+      <form>
+        <input type="text" placeholder="Title" />
+
+        <select name="category">
+          <option disabled selected value>
+            Select a category
+          </option>
+          {CATEGORIES.map(category => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Add book</button>
+      </form>
     </div>
   );
 }
