e46534fe45822f09b030b4e0811b1385d1bb5563
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-17 13:09:30 +0100

    Update BookList to display the books as an html table

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function BooksList() {
-  return(
+  const allBooks = useSelector(state => state.book);
+  return (
     <div>
-      <h3>Books List</h3>
+      <h2>Books List</h2>
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        {allBooks.map(book => (
+          <tr key={book.id}>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.category}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
-
 export default BooksList;
