b588d938df6a273cd6dd3be9cc3d6ba24e42da27
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-18 16:05:15 +0100

    Implement the CategoryFilter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+const CATEGORIES = [
+  'All',
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
+function CategoryFilter({ handleFilterChange }) {
+  return (
+    <select onChange={handleFilterChange} name="filter">
+      {CATEGORIES.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
