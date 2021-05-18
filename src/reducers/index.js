edd6efc06fa701158872bba37da3dc003d98b7b8
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-18 15:57:12 +0100

    Create a filter.js reducer which modifies filter in the Redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,21 @@
 import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'DNA',
-    category: 'Genetics',
-  },
-  {
-    id: 2,
-    title: 'RNA',
-    category: 'Biotechnology',
-  },
-];
+const initialState = {
+  book: [
+    {
+      id: 1,
+      title: 'DNA',
+      category: 'Genetics',
+    },
+    {
+      id: 2,
+      title: 'RNA',
+      category: 'Biotechnology',
+    },
+  ],
+  filter: 'All',
+};
 
 const store = createStore(rootReducer, { book: initialState });
 
