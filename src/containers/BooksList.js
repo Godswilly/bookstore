b3d6271bd621ff307ba93ed5b8d088bf7ce17562
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-20 11:35:31 +0100

    Refactor BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook, changeFilter } from '../actions/books';
+import { removeBook } from '../actions/books';
 import Book from '../components/Books';
-import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = () => {
   const dispatch = useDispatch();
   const books = useSelector((state) => state.book);
   const filter = useSelector((state) => state.filter);
   const handleRemoveBook = (id) => dispatch(removeBook(id));
-
-  const handleFilterChange = (event) => {
-    dispatch(changeFilter(event.target.value));
-  };
 
   const filterBooks = (books) => (
     filter === 'All' ? books
@@ -21,28 +16,15 @@ const BooksList = () => {
 
   return (
     <div>
-      <h2>Books List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Book ID</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-          { filterBooks(books)
-            .map((book) => (
-              <Book
-                key={book.id}
-                book={book}
-                handleRemoveBook={() => handleRemoveBook(book.id)}
-              />
-            ))}
-        </tbody>
-      </table>
-      <CategoryFilter handleFilterChange={handleFilterChange} />
+      <div>
+        {filterBooks(books).map(book => (
+          <Book
+            key={book.id}
+            book={book}
+            handleRemoveBook={() => handleRemoveBook(book.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
