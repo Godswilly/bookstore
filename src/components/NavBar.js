beb39d5d6881b75091636069aa014329f80787fe
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-20 10:40:37 +0100

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { changeFilter } from '../actions/books';
+import CategoryFilter from './CategoryFilter';
+
+const NavBar = () => {
+  const dispatch = useDispatch();
+  const handleFilterChange = (event) => {
+    dispatch(changeFilter(event.target.value));
+  };
+  return (
+    <div className="navbar main">
+      <div className="navbar content">
+        <h1>BookStore CMS</h1>
+        <h3>BOOKS</h3>
+        <h3>CATEGORIES</h3>
+        <CategoryFilter handleFilterChange={handleFilterChange} />
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
